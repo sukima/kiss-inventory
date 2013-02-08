1af30d0eda01c2e490d13afa13e75b661893dd37
Devin Weaver <suki@tritarget.org>
2013-02-08 08:48:51 -0500

    Initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KissInventory::Application.config.secret_token = '9f4ea2bed42f08a39e21cae962296966a10287057161a132fbd566fc0c1c25456ab93ec116e83945001d8896e7a726a633ef72b2cdd973e6aab04165cff0f01c'
